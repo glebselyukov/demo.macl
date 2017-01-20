91844576d4ad7b65c39d37e80c446f5d0a2bd90e
prospik <prospik@ya.ru>
2017-01-20 20:53:37 +0600

    1.0.3

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 var styles = require('./index.less');
-var static = require.context("./assets/images/", true, /^.*/);
+var statics = require.context("./assets/images/", true, /^.*/);
 var fonts = require.context("./assets/fonts/", true, /^.*/);
+var $ = require('jquery');
+
+$('.nav__burger-menu').click(function () {
+    if (!$(this).hasClass('nav__burger-menu--opened')) {
+        $('.nav__ul').fadeToggle(300, 'linear');
+    }
+    else {
+        $('.nav__ul').fadeToggle(0, 'linear');
+    }
+    $(this).toggleClass('nav__burger-menu--opened');
+});
--- webpack.config.js
+++ webpack.config.js
@@ -10,18 +10,30 @@ const paths = {
 module.exports = {
     context: path.resolve(__dirname, 'src'),
     entry: {
-        app: './index.js'
+        main: './index.js'
+        //     [
+        //     // 'webpack-dev-server/client?' + require("ip").address() + ':8080/',
+        //     // 'webpack-hot-middleware/client',
+        //     // 'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000',
+        //     './index.js'
+        // ]
     },
     module: {
         loaders: [
             {
+                test: [/\.js$/, /\.es6$/],
+                loader: 'babel-loader'
+            },
+            {
                 test: /\.css$/,
+                exclude: /node_modules/,
                 loader: ExtractTextPlugin.extract({
                     loader: 'css-loader?importLoaders=1!postcss-loader'
                 })
             },
             {
                 test: /\.less$/,
+                exclude: /node_modules/,
                 loader: ExtractTextPlugin.extract({
                     fallbackLoader: 'style-loader',
                     loader: "css-loader!less-loader!postcss-loader"
@@ -30,17 +42,22 @@ module.exports = {
             {
                 test: /\.(jpg|jpeg|gif|png)$/,
                 exclude: /node_modules/,
-                loader: 'file-loader?limit=1024&name=dist/assets/images/[name].[ext]'
+                loader: 'file-loader?publicPath=../&name=./assets/images/[name].[ext]'
             },
-            {   test: /\.[ot]tf$/,
-                loader: 'file-loader?limit=65000&mimetype=application/octet-stream&name=dist/assets/fonts/[name].[ext]'
+            {
+                test: /\.[ot]tf$/,
+                exclude: /node_modules/,
+                loader: 'file-loader?publicPath=../&name=./assets/fonts/[name].[ext]'
             }
         ]
     },
     output: {
-        path: path.resolve(__dirname, 'dist/assets')
+        path: path.resolve(__dirname, 'dist/'),
+        filename: 'bundle.js'
     },
     plugins: [
-        new ExtractTextPlugin('dist/[name].bundle.css')
+        new ExtractTextPlugin('[name].bundle.css')
+        // new webpack.HotModuleReplacementPlugin(),
+        // new webpack.NoEmitOnErrorsPlugin()
     ]
 };
